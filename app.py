937985e9a46278966e98ce84dd85448135f8432f
KevinThelly <kevin.thelly@gmail.com>
2020-01-18 00:39:19 +0530

    Due, Finished apis

--- app.py
+++ app.py
@@ -1,9 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import sqlite3
+import os.path
+
 
 app = Flask(__name__)
+app.debug=True
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+# , methods=['GET']
 @app.route('/')
-def index():
-    return render_template('home.html')
-if __name__ =='__main__':
-    app.run(debug=True)
+def api_all():
+    conn = sqlite3.connect('Database/test.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    tasks = cur.execute('SELECT * FROM TASKS;')
+    return render_template('tasks.html',Tasks=tasks)
+
+    
+@app.route("/due",methods=['GET'])
+def getdue():
+    query_parameters=request.args
+    duedate=query_parameters.get('due_date')
+    query="SELECT * FROM TASKS WHERE Due_By = '" + duedate + "';"
+    conn = sqlite3.connect('Database/test.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    tasks = cur.execute(query)
+    return render_template('tasks.html',Tasks=tasks)
+
+@app.route("/finished",methods=['GET'])
+def getfin():
+    query="SELECT * FROM TASKS WHERE Status = 'Finished' ;"
+    conn = sqlite3.connect('Database/test.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    tasks = cur.execute(query)
+    return render_template('tasks.html',Tasks=tasks)
+
+
+
+
+
+app.run()
