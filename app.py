b5ac0b1d737c6548bd9fcdf4c3310e225675fc3a
KevinThelly <kevin.thelly@gmail.com>
2020-01-18 01:44:50 +0530

    authentication basics

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 import sqlite3
 import os.path
+from datetime import datetime
+from wtforms import Form, StringField, TextAreaField, DateField
 
 
 app = Flask(__name__)
@@ -16,11 +18,12 @@ def dict_factory(cursor, row):
 # , methods=['GET']
 @app.route('/')
 def api_all():
+    form=TaskEntry(request.form)
     conn = sqlite3.connect('Database/test.db')
     conn.row_factory = dict_factory
     cur = conn.cursor()
     tasks = cur.execute('SELECT * FROM TASKS;')
-    return render_template('tasks.html',Tasks=tasks)
+    return render_template('tasks.html',Tasks=tasks, form=form)
 
     
 @app.route("/due",methods=['GET'])
@@ -42,6 +45,33 @@ def getfin():
     cur = conn.cursor()
     tasks = cur.execute(query)
     return render_template('tasks.html',Tasks=tasks)
+
+@app.route("/overdue",methods=['GET'])
+def getoverdue():
+    today=datetime.today().strftime('%Y-%m-%d')
+    query="SELECT * FROM TASKS WHERE Due_By < '" + today + "';"
+    conn = sqlite3.connect('Database/test.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    tasks = cur.execute(query)
+    return render_template('tasks.html',Tasks=tasks)
+
+
+class TaskEntry(Form):
+    task=StringField('Task')
+    date=DateField('Duedate')
+
+
+@app.route('/register',methods=['GET','POST'])
+def listtask():
+    form=TaskEntry(request.form)
+
+    # if request.method=='POST':
+    #     return 'KEVIN'
+
+    return render_template('tasks.html',form=form )
+
+
 
 
 
