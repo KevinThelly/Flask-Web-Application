d1bef2b629f2e499761b973a557cf8035cd6eb96
KevinThelly <kevin.thelly@gmail.com>
2020-01-19 19:33:26 +0530

    Updating to do list

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
 import sqlite3
 import os.path
 from datetime import datetime
-from wtforms import Form, StringField, TextAreaField, DateField
+from wtforms import Form, StringField, TextAreaField, DateField, BooleanField
+import logging
 
 
 app = Flask(__name__)
 app.debug=True
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
 
 def dict_factory(cursor, row):
     d = {}
@@ -15,14 +18,36 @@ def dict_factory(cursor, row):
     return d
 
 
+
 # , methods=['GET']
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def api_all():
     form=TaskEntry(request.form)
     conn = sqlite3.connect('Database/test.db')
+
+    if request.method=='POST':
+        # form=TaskEntry(request.form)
+        
+        t1=form.task.data
+        d1=form.date.data
+        query="select ID from TASKS ORDER BY id DESC LIMIT 1;"
+        cur=conn.cursor()
+        id=cur.execute(query).fetchall()
+
+
+        query="INSERT INTO TASKS(ID,Task,Due_By,Status) VALUES("+str(id[0][0]+1)+",'"+str(t1)+"','"+str(d1)+"','Not Started');"
+        # conn = sqlite3.connect('Database/test.db')
+        # conn.row_factory = dict_factory
+        cur= conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        conn.close()
+        return redirect(url_for('api_all'))
+    
     conn.row_factory = dict_factory
     cur = conn.cursor()
-    tasks = cur.execute('SELECT * FROM TASKS;')
+    tasks = cur.execute("SELECT * FROM TASKS where Status !='Finished' ;")
+    
     return render_template('tasks.html',Tasks=tasks, form=form)
 
     
@@ -35,7 +60,7 @@ def getdue():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     tasks = cur.execute(query)
-    return render_template('tasks.html',Tasks=tasks)
+    return render_template('onlytasks.html',Tasks=tasks)
 
 @app.route("/finished",methods=['GET'])
 def getfin():
@@ -44,7 +69,7 @@ def getfin():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     tasks = cur.execute(query)
-    return render_template('tasks.html',Tasks=tasks)
+    return render_template('onlytasks.html',Tasks=tasks)
 
 @app.route("/overdue",methods=['GET'])
 def getoverdue():
@@ -54,27 +79,12 @@ def getoverdue():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     tasks = cur.execute(query)
-    return render_template('tasks.html',Tasks=tasks)
+    return render_template('onlytasks.html',Tasks=tasks)
 
 
 class TaskEntry(Form):
     task=StringField('Task')
     date=DateField('Duedate')
-
-
-@app.route('/register',methods=['GET','POST'])
-def listtask():
-    form=TaskEntry(request.form)
-
-    # if request.method=='POST':
-    #     return 'KEVIN'
-
-    return render_template('tasks.html',form=form )
-
-
-
-
-
 
 
 app.run()
